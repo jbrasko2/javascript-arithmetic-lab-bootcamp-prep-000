86cc26213a7982dc009d27e90c996ee9f9d0cd4f
Jordan Brasko <jbrasko0508@gmail.com>
2020-10-17 18:12:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,31 +10,5 @@ function multiply () {
 function divide () {
   divide (5.0 / 2.5)
 }
-var number = 10
- 
-function add5() {
-  number += 5
-}
- 
-function divideBy3() {
-  number /= 3
-}
- 
-divideBy3()
- 
-console.log(number) // 3.333333333335
- 
-add5()
- 
-console.log(number) // 8.333333333335
- 
-// reset number
-number = 10
- 
-add5()
- 
-console.log(number) // 15
- 
-divideBy3()
- 
-console.log(number) // 5
+var a = 2
+var b = 5
